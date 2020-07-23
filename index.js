7b9ba682445227ddfe03aac440df4d9b1fbeae7c
Băeşu Rareş Gabriel <baesu_r@yahoo.com>
2020-07-23 23:02:35 +0300

    Installed modules (express, express-session, ejs template) and added a primitive structure for the express server and for the main/home page

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// Modules
+
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');	// Using the ejs view engine
+
+app.use(express.static(path.join(__dirname, 'resources'))); // resources as entry point
+
+app.use(session({
+	secret: "sessionKey",
+	resave: true,
+	saveUninitialized: false
+}));
+
+app.get("/", function(req, res) {
+	res.render("html/index");
+})
+
+
+app.listen(3000, function(){console.log("App running on port 3000")});
