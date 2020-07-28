7e9211d80de460632c5095f3dd7d1e9f3e6baf90
Băeşu Rareş Gabriel <baesu_r@yahoo.com>
2020-07-29 01:32:45 +0300

    commit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const crypto = require("crypto");
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+ 
   ssl: {
     rejectUnauthorized: false
   }
